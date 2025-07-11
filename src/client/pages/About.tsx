672ce5fe04ab9bda4bc01fc0759b532ab80784a6
skidiko <dvkobets@yandex.ru>
2025-07-11 09:18:18 +0300

    доработала deploy

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC = () => {
       <div className="row">
         <div className="col">
           <h1>About</h1>
-          <p>Author: Di</p>
+          <p>Author: Diana Kobets</p>
         </div>
       </div>
     </div>
